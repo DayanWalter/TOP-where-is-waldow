728d5e713a256c59b6ffe8dc66b261639a116e34
SynthCyraX <SynthCyraX@gmail.com>
2023-11-25 13:44:20 +0100

    Added styling to Root

--- frontend/src/components/Root.jsx
+++ frontend/src/components/Root.jsx
@@ -4,12 +4,17 @@ import styles from './Root.module.css';
 export default function Root() {
   return (
     <div className={styles.site}>
-      {/* Root styles.site */}
-      <header className={styles.header}>Root styles.header</header>
+      <header className={styles.header}>
+        <button>Restart</button>
+        <h1>PixelMania</h1>
+        <button>Leaderboard</button>
+      </header>
       <main className={styles.main}>
         <Outlet />
       </main>
-      <footer className={styles.footer}>Root styles.footer</footer>
+      <footer className={styles.footer}>
+        <h2> Synth©yrax</h2>
+      </footer>
     </div>
   );
 }
